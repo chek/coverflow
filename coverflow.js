1355b9c539ac004f4e68ab83fb5f78a2a33228cf
chekst <israel.gavriel@gmail.com>
2018-04-15 23:08:26 +0800

    settings

--- coverflow.js
+++ coverflow.js
@@ -1,9 +1,18 @@
 var Coverflow = {
     container: null,
+    loadingContainer: null,
+    settingsContainer: null,
+    widthInput: null,
+    heightInput: null,
+    spaceInput: null,
     count: 7,
     index: 3,
     pics: [],
     touchStar: null,
+    loadedImages: 0,
+    width: 320,
+    height: 240,
+    space: 20,
     twistPics: function() {
         var i;
         var zIndex = 0;    
@@ -16,6 +25,7 @@ var Coverflow = {
                     el.classList.remove("right");
                     el.classList.remove("middle");
                     el.classList.add("left");
+                    Coverflow.setImageSize(el);
                 }
             }
         } 
@@ -29,6 +39,7 @@ var Coverflow = {
                     el.classList.remove("left");
                     el.classList.remove("middle");
                     el.classList.add("right");
+                    Coverflow.setImageSize(el);
                 }
             }
         } 
@@ -37,20 +48,85 @@ var Coverflow = {
         el.classList.remove("left");
         el.classList.add("middle");
         el.classList.remove("right");
+        Coverflow.setImageSize(el);
+    },
+    setImagesSizes: function() {
+        var i;
+        Coverflow.heightInput.value = Coverflow.height;
+        Coverflow.widthInput.value = Coverflow.width;
+        for (i = 0; i < Coverflow.count; i++) { 
+            var el = Coverflow.pics[i];
+            Coverflow.setImageSize(el);
+        } 
+    },
+    setImageSize: function(el) {
+        el.style.width = Coverflow.width + 'px';    
+        el.style.height = Coverflow.height + 'px';    
+        if ( el.classList.contains("right") ) {
+            el.style.marginRight = Coverflow.space - Coverflow.width + 'px';                    
+            el.style.marginLeft = '';                    
+        }
+        if ( el.classList.contains("left") ) {
+            el.style.marginLeft = Coverflow.space - Coverflow.width + 'px';                    
+            el.style.marginRight = '';                    
+        }
+        if ( el.classList.contains("middle") ) {
+            el.style.marginLeft = '';                    
+            el.style.marginRight = '';                    
+        }
     },
     init: function() {
         Coverflow.container = document.getElementById("container");
+        Coverflow.loadingContainer = document.getElementById("loading-container");
+        Coverflow.settingsContainer = document.getElementById("settings");
+        Coverflow.widthInput = document.getElementById("width");
+        Coverflow.heightInput = document.getElementById("height");
+        Coverflow.spaceInput = document.getElementById("space");
+            
         var i;
         for (i = 1; i <= Coverflow.count; i++) { 
             var img = document.createElement("img");
-            img.src = "https://loremflickr.com/320/240?random=" + i;            
             Coverflow.container.appendChild(img);            
+            img.src = "https://loremflickr.com/320/240?random=" + i;     
+            img.onload = function() {
+                Coverflow.loadedImages += 1;
+                if (Coverflow.loadedImages === Coverflow.count) {
+                    Coverflow.loadingContainer.style.display = 'none';    
+                    Coverflow.container.style.display = 'block';    
+                    Coverflow.settingsContainer.style.display = 'block';    
+                    Coverflow.setImagesSizes();
+                }
+            };                   
             Coverflow.pics.push(img);
         }        
         Coverflow.twistPics();
 
 
 
+        Coverflow.spaceInput.onchange = function(e) {
+            e.preventDefault();
+            Coverflow.space = Coverflow.spaceInput.value;
+            Coverflow.setImagesSizes();
+        }
+        Coverflow.widthInput.onchange = function(e) {
+            e.preventDefault();
+            var neWidth = Coverflow.widthInput.value;
+            var coef = 320/240;
+            var newHeight = neWidth/coef;
+            Coverflow.width = neWidth;
+            Coverflow.height = newHeight;
+            Coverflow.setImagesSizes();
+        }
+        Coverflow.heightInput.onchange = function(e) {
+            e.preventDefault();
+            var newHeight = Coverflow.heightInput.value;
+            var coef = 320/240;
+            var neWidth = coef/newHeight;
+            Coverflow.width = neWidth;
+            Coverflow.height = newHeight;
+            Coverflow.setImagesSizes();
+        }
+        //Coverflow.heightInput = document.getElementById("height");
 
         Coverflow.container.onmousedown = function(e){
             e.preventDefault();
